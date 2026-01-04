9d8cdaaf447446465fcbaf106fd09e177ebbcd9a
DrJanPanNees <JanPanNees@yahoo.com>
2026-01-04 12:48:14 +0100

    feat: Test af WatchTower-opdatering

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, WatchTower! Dette er endnu en test.");
+
+Console.WriteLine("Hello, WatchTower! Dette er endnu en test 2.");
